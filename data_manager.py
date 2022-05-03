c3f9427485fb52b4774bf30626168831a81688aa
MagdalenaHol <magdalena.holda2@gmail.com>
2022-05-03 12:42:28 +0200

    added searching applicants by name

--- data_manager.py
+++ data_manager.py
@@ -21,6 +21,31 @@ def get_mentors_by_last_name(cursor, last_name):
     query = """
         SELECT first_name, last_name, city
         FROM mentor
+        WHERE lower(last_name) = lower(%(last_name)s)
         ORDER BY first_name"""
-    cursor.execute(query)
+    l_name = {'last_name': last_name}
+    cursor.execute(query, l_name)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_city_name(cursor, city):
+    query = """
+        SELECT first_name, last_name, city
+        FROM mentor
+        WHERE city = (%(city)s)"""
+    city_ = {'city': city}
+    cursor.execute(query, city_)
+    return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_applicant_data_by_name(cursor, name):
+    query = """
+        SELECT first_name, last_name, phone_number
+        FROM applicant
+        WHERE lower(first_name) = lower(%(name)s)"""
+    applicant_ = {'name': name}
+
+    cursor.execute(query, applicant_)
     return cursor.fetchall()
--- main.py
+++ main.py
@@ -13,15 +13,29 @@ def index():
 @app.route('/mentors')
 def mentors_list():
     mentor_name = request.args.get('mentor-last-name')
+    city_name = request.args.get('city-name')
 
     if mentor_name:
-        mentor_details = data_manager.get_mentors_by_last_name(mentor_name)
+        mentor_info = data_manager.get_mentors_by_last_name(mentor_name)
+    elif city_name:
+        mentor_info = data_manager.get_city_name(city_name)
     else:
-        mentor_details = data_manager.get_mentors()
+        mentor_info = data_manager.get_mentors()
 
     # We get back a list of dictionaries from the data_manager (for details check 'data_manager.py')
 
-    return render_template('mentors.html', mentors=mentor_details)
+    return render_template('mentors.html', mentors=mentor_info, cites=mentor_info)
+
+
+@app.route('/applicant-phone')
+def applicant_searching():
+    applicant_first_name = request.args.get('applicant-first-name')
+    applicant_last_name = request.args.get('applicant-last-name')
+
+    if applicant_first_name or applicant_last_name:
+        applicant_info = data_manager.get_applicant_data_by_name(applicant_first_name)
+
+    return render_template('index.html', applicants=applicant_info)
 
 
 if __name__ == '__main__':
